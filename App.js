9b60db8876c2501b2da46fbba83631a265701a13
freshfoxstudio-bit <freshfoxstudio@gmail.com>
2026-01-27 13:45:14 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminPage from './src/components/adminpage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to The Thirsty Creator</h1>
+      <AdminPage />
+    </div>
+  );
+}
+
+export default App;
