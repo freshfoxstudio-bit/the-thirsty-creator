4ffdbafcda68f12ba8d585c8d0e5e2be0ac50542
freshfoxstudio-bit <freshfoxstudio@gmail.com>
2026-01-27 20:05:26 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // This looks for App.js
+import './styles.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
